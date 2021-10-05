804fc79b2d488969c357089180475c93a820da16
Mr-Smitt <jacob@paal.org>
2021-10-05 06:14:26 +0200

    Lagt til kommentar

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,11 @@
 public class HelloWorld
 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World"); //Sender text hello world
 
         System.out.println("Teller til 10");
-        for (int i=1; i<=10; ++i) {
-            System.out.println(i);
+        for (int i=1; i<=10; ++i) { //for løkke som vil kjøres 10 ganger og øker i med 1 hver gang
+            System.out.println(i); //her sender vi ut i til println som burde være 10
         }
     }
 
